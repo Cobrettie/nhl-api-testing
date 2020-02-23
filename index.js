64917bb33ca5ccebd8bdcee0a87a03ebd4c1dfbf
Cobrettie <cobrettiegarner3@gmail.com>
2020-02-23 15:14:07 -0500

    test api, get request successful,

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // test if i can even access api
+axios
+  .get('https://statsapi.web.nhl.com/api/v1/teams')
+  .then(response => {
+    console.log(response);
+  })
+
+  let test = function() {console.log('hey')};
+
+  test();
