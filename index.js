7f339dc804412aaba557cd13216bf6d62efcef0a
Cobrettie <cobrettiegarner3@gmail.com>
2020-02-23 15:42:39 -0500

    individual team names appearing

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
-function cardCreator() {
+function cardCreator(team) {
   const mainCardDiv = document.createElement('div');
   mainCardDiv.classList.add('mainCardContain');
+
+  const singleCard = document.createElement('div');
+  singleCard.classList.add('singleCard');
+  singleCard.textContent = team;
+
+  mainCardDiv.appendChild(singleCard);
 
   return mainCardDiv;
 } 
 
 let pageContainer = document.querySelector('.page-container');
 
-// axios
-//   .get('https://statsapi.web.nhl.com/api/v1/teams')
-//   .then(response => {
-//     console.log(response.data.teams);
-//   })
+axios
+  .get('https://statsapi.web.nhl.com/api/v1/teams')
+  .then(response => {
+    let teams = response.data.teams;
+    teams.forEach(item => {
+      let card = cardCreator(item.name);
+      console.log(teams);
+      pageContainer.appendChild(card);
+    })
+  })
